81fa1d9781bebbabf2a323c0d66bf9c6b7948074
Suresh Jat <sureshjatcode@gmail.com>
2024-08-28 23:36:48 +0530

    Impliment Linked List using collection framwork

---
+++ List/Link_List_CF.java
@@ -0,0 +1,29 @@
+package List;
+
+import java.util.LinkedList;
+
+public class Link_List_CF {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+        list.addFirst("This");
+
+        list.addLast("Link");
+        list.add("List"); // Both addLast and add are same 
+        System.out.println(list);
+
+        list.removeFirst();
+        list.remove(); // Both removeFirst and remove are same 
+        list.removeLast();
+        System.out.println(list);
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " -> ");
+        }
+        System.out.println("null");
+        
+    }
+}
