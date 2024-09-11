607fbb2464af85c92cc22b902eea6ec169fee781
Suresh Jat <sureshjatcode@gmail.com>
2024-09-11 18:06:02 +0530

    Stack & queue

---
+++ Stack & queue/stack.java
@@ -0,0 +1,67 @@
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class StackDS {
+    Node head;
+
+    // Add element at top of the Stack
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    // Delete element at the top of the Stack 
+    public void pop() {
+        if (head == null) {
+            System.out.println("Stack is empty!");
+            return;
+        }
+        head = head.next;
+    }
+
+    // Get the value of top element which is at the top of Stack
+    public int peak() {
+        if (head == null) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        return head.data;
+    }
+
+
+    public void printStack() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
+
+public class stack {
+    public static void main(String[] args) {
+        StackDS stk = new StackDS();
+        stk.push(1);
+        stk.push(3);
+        
+        stk.pop();
+        
+        System.out.println(stk.peak());
+
+        // stk.printStack();
+    }
+}
