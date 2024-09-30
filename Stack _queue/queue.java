c706369ee9cdc02e3aa25aa5ac9fc1e0d1c74b91
Suresh Jat <sureshjatcode@gmail.com>
2024-09-30 21:39:48 +0530

    Queue using array

--- Stack _queue/queue.java
+++ Stack _queue/queue.java
@@ -1,67 +1,67 @@
-// CORRECTION 
-
 public class queue {
     static class Queue {
 
         static int array[];
         static int size;
-        static int front = 0;
         static int rear = -1;
 
         Queue(int size) {
-            this.size = size;
+            Queue.size = size;
             array = new int[size];
         }
 
         // isEmpty function 
         boolean isEmpty() {
-            if (front == rear + 1) {
-                return true;
-            }
-            return false;
+            return rear == -1;
         }
         
-        // adding element in queue
+        // enqueue
         void add(int data) {
             if (rear == size - 1) {
-                System.out.println("No space in Queue to add element!");
+                System.out.println("Queue is Full!");
                 return;
             }
             rear++;
             array[rear] = data;
         }
 
-        // removing element from queue 
-        void remove() {
+        // dequeue
+        int remove() {
             if (isEmpty()) {
-                System.out.println("No element left!");
-                return;
-            }
-            front++;
-        }
-
-        // peek element in stack 
-        int peek() {
-            if (isEmpty()) {
-                System.out.println("No element left!");
+                System.out.println("Empty Queue!");
                 return -1;
             }
-            return array[front];
+            int front = array[0];
+
+            for (int i = 0; i < rear; i++) {
+                array[i] = array[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        // peek
+        int peek() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue!");
+                return -1;
+            }
+            return array[0];
         }
     }
     public static void main(String[] args) {
-        Queue q = new Queue(5);
+        Queue q = new Queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
         q.add(4);
         q.add(5);
-        q.add(6);
-        q.add(7);
-        
+
         for (int i = 0; i < 5; i++) {
             System.out.println(q.peek());
             q.remove();
         }
+        
+
     }
 }
